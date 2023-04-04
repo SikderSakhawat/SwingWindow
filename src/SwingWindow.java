83505a100052f4501f8836830fbe59abdb5d9c7b
unknown <BT_4N2_08@BKTECH.LOCAL>
2023-04-04 12:25:00 -0400

    Initial commit

---
+++ src/SwingWindow.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SwingWindow extends JFrame implements ActionListener {
+    private JPanel mainPanel;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JButton ADDButton;
+
+    public void actionPerformed(ActionEvent e) {
+        
+    }
+}
